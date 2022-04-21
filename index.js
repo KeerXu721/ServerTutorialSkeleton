373115c8fe1f65f8f2742e412853b9eb8d7de5d2
KeerXu721 <keerxu@brandeis.edu>
2022-04-21 09:30:54 -0400

    change

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const PORT = 3000;
 app.listen(PORT, () => console.info('Server has started on', PORT));
 
 let newShip = require("./routes/newShip");
+let getShip = require("./routes/getShip");
+let byChar = require("./routes/getShip/secondaryBattery");
 
 // on every request, parse the request body using this library.
 app.use(bodyParser.json());
 // on a request to /, use the newShip code.
 // note that the entire URL including the one declared in the file must match.
 app.use("/",newShip);
+app.use("/",getShip);
+app.use("/",byChar);
 
 // If you type this instead of CTRL+C CTRL+V, note how Visual Studio Code pops up a mini file explorer when you type require("./).
 const DB = require("./db");
